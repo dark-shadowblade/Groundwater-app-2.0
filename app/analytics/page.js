8c2fe4673e06c45a68a2c95013ec98fd633db53f
SIH-groundwater <rituaassaa@gmail.com>
2025-09-19 15:09:35 +0530

    Update page.js

--- app/analytics/page.js
+++ app/analytics/page.js
@@ -1,3 +1,8 @@
 export default function Analytics() {
-  return <div style={{ padding: '2rem' }}>Analytics Page - Coming Soon</div>;
+  return (
+    <div style={{ padding: '2rem', paddingBottom: '5rem' }}>
+      <h1>Analytics Page</h1>
+      <p>Data analytics and charts will be displayed here.</p>
+    </div>
+  );
 }
