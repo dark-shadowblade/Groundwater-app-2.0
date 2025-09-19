f725855e58536dd838cf1fc371762edff2d881d4
SIH-groundwater <rituaassaa@gmail.com>
2025-09-19 15:09:59 +0530

    Update page.js

--- app/alerts/page.js
+++ app/alerts/page.js
@@ -1,3 +1,8 @@
 export default function Alerts() {
-  return <div style={{ padding: '2rem' }}>Alerts Page - Coming Soon</div>;
+  return (
+    <div style={{ padding: '2rem', paddingBottom: '5rem' }}>
+      <h1>Alerts Page</h1>
+      <p>Detailed alert information will be displayed here.</p>
+    </div>
+  );
 }
